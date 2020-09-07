484e446c11cf436811998284a80ab8eab462a104
Alisha Rich <anhelmoders@gmail.com>
2020-09-07 11:50:59 +0300

    Optimization

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,18 @@ const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
-// const minify = require("gulp-csso");
-// const rename = require("gulp-rename");
+const minify = require("gulp-csso");
+const rename = require("gulp-rename");
 const sync = require("browser-sync").create();
-// const imagemin = require("gulp-imagemin");
-// const webp = require("gulp-webp");
-// const svgstore = require("gulp-svgstore");
-// const del = require("del");
-
-// Styles
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
+const posthtml = require("gulp-posthtml");
+const include = require("posthtml-include");
+const htmlmin = require("gulp-htmlmin");
+const uglify = require("gulp-uglify");
+const pipeline = require("readable-stream").pipeline;
 
 const styles = () => {
   return gulp.src("source/sass/style.scss")
@@ -22,28 +25,38 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
-    // .pipe(minify())
-    // .pipe(rename("style.min.css"))
-    // .pipe(sourcemap.write("."))
-    // .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
 
-// const html = () => {
-//   return gulp.src("source/*.html")
-//     .pipe(gulp.dest("build"));
-// }
-//
-// exports.html = html;
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([include()
+    ]))
+    .pipe(htmlmin())
+    .pipe(gulp.dest("build"));
+}
 
-// Server
+exports.html = html;
+
+const jsmin = () => {
+  return pipeline(gulp.src("source/js/*.js"),
+    (uglify())
+    .pipe(gulp.dest("build/js"))
+  );
+}
+
+exports.jsmin = jsmin;
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: "source"  // baseDir: "build"
+      baseDir: "build"
     },
     cors: true,
     notify: false,
@@ -54,8 +67,6 @@ const server = (done) => {
 
 exports.server = server;
 
-// Watcher
-
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
@@ -65,55 +76,54 @@ exports.default = gulp.series(
   styles, server, watcher
 );
 
-// const images = () => {
-//   return gulp.src("source/img/**/*.{png,jpg,svg}")
-//     .pipe(imagemin ([
-//       imagemin.optipng({optimizationLevel: 3}),
-//       imagemin.mozjpeg({progressive: true}),
-//       imagemin.svgo()
-//     ]))
-//     .pipe(gulp.dest("build/img"));
-// }
-//
-// exports.images = images;
-//
-//
-// const convertWebp = () => {
-//   return gulp.src("source/img/**/*.{png,jpg}")
-//     .pipe(webp({quality: 90}))
-//     .pipe(gulp.dest("build/img"));
-// }
-//
-// exports.webp = convertWebp;
-//
-// const sprite = () => {
-//   return gulp.src("source/img/sprite-*.svg")
-//     .pipe(svgstore({inlineSvg: true}))
-//     .pipe(rename("sprite.svg"))
-//     .pipe(gulp.dest("build/img"));
-// }
-//
-// exports.sprite = sprite;
-//
-// const copy = () => {
-//   return gulp.src([
-//     "source/fonts/**/*.{woff,woff2}",
-//     "source/img/**",
-//     "source/js/**"
-//   ], {
-//     base: "source"
-//   })
-//   .pipe(gulp.dest("build"));
-// }
-//
-// exports.copy = copy;
-//
-// const clean = () => {
-//   return del("build");
-// }
-//
-// exports.clean = clean;
-//
-// exports.build = gulp.series(
-//   clean, copy, styles, images, convertWebp, sprite, html
-// );
+const images = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin ([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+}
+
+exports.images = images;
+
+const convertWebp = () => {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"));
+}
+
+exports.webp = convertWebp;
+
+const sprite = () => {
+  return gulp.src("source/img/sprite-*.svg")
+    .pipe(svgstore({inlineSvg: true}))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+}
+
+exports.sprite = sprite;
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+}
+
+exports.copy = copy;
+
+const clean = () => {
+  return del("build");
+}
+
+exports.clean = clean;
+
+exports.build = gulp.series(
+  clean, copy, styles, images, convertWebp, sprite, html, jsmin
+);
---
+++ source/js/map.js
@@ -0,0 +1,19 @@
+ymaps.ready(function() {
+    const map = new ymaps.Map("map", {
+        center: [59.938635, 30.323118],
+        zoom: 17
+    });
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            "<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>"
+        ),
+        myPlacemark = new ymaps.Placemark(map.getCenter(), {
+            hintContent: "Мы находимся здесь"
+        }, {
+            iconLayout: "default#image",
+            iconImageHref: "img/icon-map-pin.svg",
+            iconImageSize: [67, 100],
+            iconImageOffset: [-45, -100]
+        }),
+        map.geoObjects
+        .add(myPlacemark)
+});
